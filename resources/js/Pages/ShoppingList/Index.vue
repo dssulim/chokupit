ae3e14b97177e2123fc5e913e64b6f221754eae5
Dymoc <dymoc@mail.ru>
2023-01-15 12:32:27 +0300

    Добавил переход к списку продуктов

--- resources/js/Pages/ShoppingList/Index.vue
+++ resources/js/Pages/ShoppingList/Index.vue
@@ -37,12 +37,14 @@ const myList = defineProps({
           <div class="hover:cursor-pointer w-full flex items-center justify-between mb-2 pb-2 border-b border-stone-400"
             v-for= "(list, idx) in myList.lists" :key="idx"
           >
+          <a :href="route('shoppingLists.show', list.id)">
             <div class="flex items-center justify-start w-[40%]">
               {{ list.shopping_list_date }}
             </div>
             <div class="flex items-center justify-start w-[40%]">
               {{ list.list_name }}
             </div>
+          </a>
             <div class="hover:cursor-pointer flex items-center justify-end">
               <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-right" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"/>
